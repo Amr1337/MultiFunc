2b8dee307615293d5b49453c01a46a44797c7cf4
Amr <39915155+Amr1337@users.noreply.github.com>
2021-11-13 16:57:47 +0300

    Add files via upload

---
+++ newProject.py
@@ -0,0 +1,22 @@
+
+def countlist(lst):
+    even = 0
+    odd = 0
+
+    for i in lst:
+        if i % 2 ==0:
+            even +=i
+
+        else:
+            odd+=i
+    return even, odd
+
+
+
+
+lst=[1,2,3,4,5,6,7]
+
+even, odd=countlist(lst)
+
+print(even)
+print(odd)
